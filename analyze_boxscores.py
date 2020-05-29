55d62ee9cb01e185588c68aa301307f689a2515e
Álvaro Peris <lvapeab@gmail.com>
2020-05-29 12:17:44 +0200

    Metadata

--- analyze_boxscores.py
+++ analyze_boxscores.py
@@ -1,16 +1,17 @@
+import glob
 import lxml.html as lh
 import os
 import pandas as pd
 import requests
 from urllib.parse import urlparse
-from typing import Optional, List
+from typing import Dict, Optional, List
 
-from feb_stats.parser import get_elements, game_stats_elements_to_df
+from feb_stats.parser import get_elements, game_stats_elements_to_df, get_game_metadata
 from feb_stats.game_stats_transforms import parse_game_stats_df
 from feb_stats.utils import dataframe_to_excel
 
 def parse_game_stats(link: str,
-                     ids: List[Optional[str]]=None) -> pd.DataFrame:
+                     ids: List[Optional[str]]=None) -> Dict[str, pd.DataFrame]:
     ids = ids or [
         ('//table[@id="jugadoresLocalDataGrid"]//tr', True),
         ('//table[@id="jugadoresVisitanteDataGrid"]//tr', False)
@@ -30,24 +31,31 @@ def parse_game_stats(link: str,
     else:
         raise ValueError(f'Unable to find the resource {link} (not a valid URL nor an existing file.)')
 
-    dfs = []
+    game_stats = {}
+    game_stats['metadata'] = get_game_metadata(doc)
     for (id, local) in ids:
         elements = get_elements(doc,
                                 id)
+        key = 'local_boxscore' if local else 'visitante_boxscore'
         if elements:
             ori_df = game_stats_elements_to_df(elements,
                                               initial_row=2,
                                               n_elem=18)
             df = parse_game_stats_df(ori_df,
                                      local_team=local)
-            dfs.append(df)
-    return df
+            game_stats[key] = df
+    return game_stats
 
+def aggregate_boxscores(boxscores_dir):
+    for link in glob.iglob(os.path.join(boxscores_dir, '*.html'), recursive=False):
+        parsed_dfs = parse_game_stats(link)
+    print(parsed_dfs)
 
 if __name__ == '__main__':
     # link = 'http://competiciones.feb.es/estadisticas/Estadisticas.aspx?g=39&t=0'
     link = '/home/lvapeab/projects/feb-stats/test_artifacts/game_sheet.html'
-    parsed_df = parse_game_stats(link)
-    dataframe_to_excel(parsed_df,
-                       './df.xlsx',
-                       sheet_name='Game Sheet')
+    aggregate_boxscores('/home/lvapeab/projects/feb-stats/test_artifacts/')
+    # parsed_df = parse_game_stats(link)
+    # dataframe_to_excel(parsed_df,
+    #                    './df.xlsx',
+    #                    sheet_name='Game Sheet')
--- feb_stats/parser.py
+++ feb_stats/parser.py
@@ -1,21 +1,56 @@
 import pandas as pd
-from typing import List
+from lxml.html import HtmlElement, Element
+from typing import Dict, List
 
 
 def parse_str(input_str: str):
     return ' '.join(input_str.replace('\n', '').replace('\t', '').replace('\r', '').replace(',', '.').split())
 
 
-def get_elements(doc: str,
-                 id: str):
+def get_elements(doc: Element,
+                 id: str) -> HtmlElement:
     # Parse data by id
     table_elements = doc.xpath(id)
     return table_elements
 
 
-def cum_stats_elements_to_df(tr_elements: List[str],
-                             initial_row:int=2,
-                             n_elem: int=0
+def get_game_metadata(doc: Element) -> Dict[str, str]:
+    # Parse data by id
+
+    # '//table[@id="jugadoresLocalDataGrid"]//tr'
+
+    fecha = doc.xpath('//span[@id="fechaLabel"]')
+    hora = doc.xpath('//span[@id="horaLabel"]')
+
+    liga = doc.xpath('//span[@id="paginaTitulo_ligaLabel"]')
+    temporada = doc.xpath('//span[@id="paginaTitulo_temporadaLabel"]')
+    equipo_local = doc.xpath('//a[@id="equipoLocalHyperLink"]')
+    resultado_local = doc.xpath('//span[@id="resultadoLocalLabel"]')
+    equipo_visitante = doc.xpath('//a[@id="equipoVisitanteHyperLink"]')
+    resultado_visitante = doc.xpath('//span[@id="resultadoVisitanteLabel"]')
+
+    arbitro_principal = doc.xpath('//span[@id="arbitroPrincipalLabel"]')
+    arbitro_auxiliar = doc.xpath('//span[@id="arbitroAuxiliarLabel"]')
+
+    metadata_dict = {
+        'fecha': parse_str(fecha[0].text_content()),
+        'hora': parse_str(hora[0].text_content()),
+        'liga': parse_str(liga[0].text_content()),
+        'temporada': parse_str(temporada[0].text_content()),
+        'equipo_local': parse_str(equipo_local[0].text_content()),
+        'resultado_local': parse_str(resultado_local[0].text_content()),
+        'equipo_visitante': parse_str(equipo_visitante[0].text_content()),
+        'resultado_visitante': parse_str(resultado_visitante[0].text_content()),
+        'arbitro_principal': parse_str(arbitro_principal[0].text_content()),
+        'arbitro_auxiliar': parse_str(arbitro_auxiliar[0].text_content()),
+    }
+
+    return metadata_dict
+
+
+def cum_stats_elements_to_df(tr_elements: List[Element],
+                             initial_row: int = 2,
+                             n_elem: int = 0
                              ) -> pd.DataFrame:
     col = []
     i = 0
@@ -52,13 +87,12 @@ def cum_stats_elements_to_df(tr_elements: List[str],
     data_dict = {title: column for (title, column) in col}
     df = pd.DataFrame(data_dict)
     return df
-
 
 
 def game_stats_elements_to_df(tr_elements: List[str],
-                             initial_row:int=2,
-                             n_elem: int=0
-                             ) -> pd.DataFrame:
+                              initial_row: int = 2,
+                              n_elem: int = 0
+                              ) -> pd.DataFrame:
     col = []
     i = 0
     # For each row, store each first element (header) and an empty list
@@ -94,7 +128,3 @@ def game_stats_elements_to_df(tr_elements: List[str],
     data_dict = {title: column for (title, column) in col}
     df = pd.DataFrame(data_dict)
     return df
-
-
-
-
