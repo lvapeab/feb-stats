65629b5674c5d030b0c03c1ef13b40cf222e2d4b
Álvaro Peris <lvapeab@gmail.com>
2020-10-31 11:47:32 +0100

    Add Flask web (not working yet!)

--- python/web/webapp.py
+++ python/web/webapp.py
@@ -3,10 +3,6 @@ import glob
 import yaml
 
 from datetime import datetime
-import grpc
-
-from flask_sqlalchemy import SQLAlchemy
-
 from flask import (
     Flask,
     url_for,
@@ -17,51 +13,41 @@ from flask import (
     redirect,
 )
 from werkzeug.utils import secure_filename
-
+from typing import List
 from python.service.api import FebStatsServiceServicer
 from python.service.handler import SimpleLeagueHandler
-from python.service.codegen.feb_stats_pb2 import GetFebStatsRequest
+from python.service.server import feb_stats_pb2
 
 if os.environ.get("SERVER_ENVIRONMENT", "") == "PRODUCTION":
-    config_filename = "config/production.yaml"
+    config_filename = "python/web/config/production.yaml"
 else:
-    config_filename = "config/local.yaml"
+    config_filename = "python/web/config/local.yaml"
 
 with open(config_filename) as f:
     config = yaml.safe_load(f)
 
-
 app = Flask(__name__)
+
 app.config["UPLOAD_FOLDER"] = config.get("upload_folder", "uploads")
 app.config["MAX_CONTENT_LENGTH"] = config.get("max_content_length", 16 * 1024 * 1024)
-
-db_config = config["database"]
-db_auth = (
-    f'{db_config["username"]}:{db_config["password"]}@{db_config["hostname"]}'
-    if db_config["username"]
-    else ""  #  os.path.dirname(os.path.realpath(__file__))
-)
-
-SQLALCHEMY_DATABASE_URI = f"{db_config['engine']}://{db_auth}/{db_config['db_name']}"
-
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db = SQLAlchemy(app)
-db.app = app
-from python.web.db import Team
+ports_config = config["ports"]
 
 
 @app.route("/")
 def index(name=None):
-    url_for("static", filename="main.css")
-    return render_template("index.hbs", name=name)
+    # url_for("static", filename="python/web/static/main.css")
+    return render_template("index.html", name=name)
 
 
 def allowed_file_extension(filename, allowed_extensions=None):
     allowed_extensions = allowed_extensions or {"html"}
     return "." in filename and filename.rsplit(".", 1)[1].lower() in allowed_extensions
+
+
+def read_file(file):
+    with open(file, mode="rb") as f:
+        read_f = f.read()
+    return read_f
 
 
 @app.route("/upload", methods=["POST", "GET"])
@@ -76,11 +62,13 @@ def upload() -> str:
 
         if file and allowed_file_extension(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+            print(f"Saving file to {filepath}")
+            file.save(filepath)
             return "OK"
-    error = "Invalid username/password"
+    error = "Invalid /upload request."
     # the code below is executed if the request method was GET
-    return render_template("login.html", error=error)
+    return render_template("index.html", error=error)
 
 
 def read_file(file):
@@ -89,37 +77,37 @@ def read_file(file):
     return read_f
 
 
-class ContextStub:
-    def invocation_metadata(self):
-        return [("tenant", "test")]
-
-
-@app.route("/analyze", methods=["POST", "GET"])
-def analyze():
-    boxscores = [
+def read_boxscores() -> List[bytes]:
+    return [
         read_file(filename)
         for filename in glob.glob(os.path.join(app.config["UPLOAD_FOLDER"], "*html"))
     ]
-    with grpc.insecure_channel("localhost:50001") as channel:
-        grpc_request = GetFebStatsRequest(boxscores=boxscores,)
-        service = FebStatsServiceServicer(
-            SimpleLeagueHandler(address="localhost:50001")
-        )
-        grpc_response = service.GetFebStats(grpc_request, ContextStub())
 
-        # Commit e.g. Teams in the database
-        for team in grpc_response.teams:
-            t = Team(str(team))
-            db.session.add(t)
-        db.session.commit()
 
-        # Output the data sheet
-        output_filename = datetime.now().strftime("%d/%m/%Y_%H:%M")
-        response = make_response(grpc_response.sheet)
-        response.headers["Content-Type"] = "application/vnd.ms-excel"
-        response.headers[
-            "Content-disposition"
-        ] = f"attachment; filename=estadisticas_{output_filename}.xlsx"
-        response.headers["Content-Length"] = len(grpc_response.sheet)
+def remove_boxscores() -> None:
+    for filename in glob.glob(os.path.join(app.config["UPLOAD_FOLDER"], "*html")):
+        os.remove(filename)
 
+
+@app.route("/analyze", methods=["POST"])
+def analyze():
+    boxscores = read_boxscores()
+    grpc_address = f"{ports_config.get('grpc_address', 'localhost')}:f{ports_config.get('grpc_port', '50001')}"
+    grpc_request = feb_stats_pb2.GetFebStatsRequest(boxscores=boxscores,)
+    service = FebStatsServiceServicer(SimpleLeagueHandler(address=grpc_address))
+    grpc_response = service.GetFebStats(grpc_request, None)
+
+    # Output the data sheet
+    output_filename = datetime.now().strftime("%d/%m/%Y_%H:%M")
+    response = make_response(grpc_response.sheet)
+    response.headers["Content-Type"] = "application/vnd.ms-excel"
+    response.headers[
+        "Content-disposition"
+    ] = f"attachment; filename=estadisticas_{output_filename}.xlsx"
+    response.headers["Content-Length"] = len(grpc_response.sheet)
+    # remove_boxscores()
     return response
+
+
+if __name__ == "__main__":
+    app.run()
